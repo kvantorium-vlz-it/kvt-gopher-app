0e7141dfcfaa017049113d7e1aea01736e56b984
derloky <semenpostnikov093@gmail.com>
2025-04-21 19:44:27 +0300

    Add strapi

---
+++ apps/cms/src/api/user-map-progress/controllers/user-map-progress.ts
@@ -0,0 +1,7 @@
+/**
+ * user-map-progress controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::user-map-progress.user-map-progress');
---
+++ apps/cms/src/api/user-map-progress/services/user-map-progress.ts
@@ -0,0 +1,7 @@
+/**
+ * user-map-progress service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::user-map-progress.user-map-progress');
