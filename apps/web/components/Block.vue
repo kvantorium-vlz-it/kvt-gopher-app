be43ac18c7a68f9cc50d2127143b53641428d485
derloky <semenpostnikov093@gmail.com>
2025-04-30 19:20:54 +0300

    ебные новые кмпнты

---
+++ apps/web/components/Block.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="block">
+    </div>
+</template>
+<style scoped>
+
+.block{
+    padding: 20px;
+    isolation: isolate;
+    overflow: hidden;
+    border-radius: 16px;
+}
+</style>
---
+++ apps/web/components/ButtonAction.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+<template>
+    <button class="button">
+        <TwentyText>вперёд!</TwentyText>
+    </button>
+</template>
+<style scoped>
+
+.button{
+    padding: 7px 44px;
+    border-radius: 16px;
+    background-color: #3C69FF;
+    display: inline-flex;
+    justify-content: center;
+    border:none ;
+    text-decoration: none;
+}
+
+</style>
