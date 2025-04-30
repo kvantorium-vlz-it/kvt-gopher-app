d27b3e0f07013d0eacfa527c480e09ca47cd290e
derloky <semenpostnikov093@gmail.com>
2025-04-30 19:15:54 +0300

    auth with google

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -4,18 +4,40 @@
     {{JSON.stringify(response,null, 2)  }} -->
     <Map :points="response.data"></Map>
   </div>
+
+  <button @click="onClick">
+    Войти через Google
+  </button>
+  <button @click="onLogout">
+    Выйти
+  </button>
+
 </template>
 
-<script setup lang="ts">
-  const { find } = useStrapi()
 
+<script setup lang="ts">
+  const { find  } = useStrapi()
+  const router = useRouter()
   const response = ref()
  response.value = await find('locations')
-console.log(JSON.stringify(response.value.data,null, 2) );
-const { authenticateProvider } = useStrapiAuth()
-const route = useRoute()
+ const route = useRoute()
+ const { authenticateProvider, fetchUser } = useStrapiAuth()
 
-await authenticateProvider('google', route.query.access_token)
+ const { getProviderAuthenticationUrl, logout } = useStrapiAuth()
 
+const onClick =  async() => {
+  window.location.href = getProviderAuthenticationUrl('google')
+  await authenticateProvider('google', route.query.access_token as string)
+  await fetchUser() // Обновляем данные пользователя
+}
+const onLogout = async () => {
+  const { fetchUser } = useStrapiAuth()
+
+  const user = await fetchUser()
+  console.log(user);
+  
+}
 </script>
+
+
 
