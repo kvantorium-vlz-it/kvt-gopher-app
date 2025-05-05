7cdd06079866ed55d0ab7b21205f689ed2a29f96
derloky <semenpostnikov093@gmail.com>
2025-05-05 20:01:50 +0300

    .

--- apps/web/components/Menu.vue
+++ apps/web/components/Menu.vue
@@ -2,12 +2,6 @@
 </script>
 <template>
     <div class="city-area">
-        <TwentyText>город</TwentyText>
-        <Block class="city-area-card">
-            <TwentyText class="white">Ваши граждане скучают <br>по своему мэру!</TwentyText>
-            <ButtonAction class="white">вперёд!</ButtonAction>
-            <!-- <img src="../public/images/Semen.png" class="city-area-card-background"> -->
-        </Block>
         
     </div>
 </template>
