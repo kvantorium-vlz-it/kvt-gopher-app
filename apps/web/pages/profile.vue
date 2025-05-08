1a135f1e87f2aea61e3a94702f1e04a96a1c9c44
Feodor <fdomanov@inbox.ru>
2025-05-09 02:35:35 +0300

    чё-то с профилем поделал

--- apps/web/pages/profile.vue
+++ apps/web/pages/profile.vue
@@ -1,52 +1,43 @@
 <template>
     <div class="main">
-        <TheHeader username="sadads"/>
+        <TheHeader username="sadads" />
+        <div class="cituselect">
+            <a href="" style="text-decoration: none;">
+                выбор города здесь
+            </a>
+        </div>
         <div class="display">
-            <HomeHeader>
-                <template #left>
 
-                    <MenuButton title="">
-                        <template #icon>
-                            <Icon name=""/>
-                        </template>
-                    </MenuButton>
+            <Section class="story-section">
+                статистика
+                <div>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9c1904VH9PR2xYqTgQ0YAg9sSj2UAXtrbtQ&s" alt="">
+                </div>
+            </Section>
 
-                </template>
-                <template #middle>
-                        <MenuButton title="">
-                        <template #icon>
-                            <Icon name=""/>
-                        </template>
-                    </MenuButton>
+            <Section class="city-section">
+                твой чушпан
+                <div>
+                    <img src="https://img.pikbest.com/png-images/qiantu/original-cute-cartoon-gopher_2736241.png!sw800" alt="" width="208">
+                </div>
+            </Section>
 
-                </template>
-            <template #right>
-                    <MenuButton title="">
-                    <template #icon>
-                            <Icon name=""/>
-                        </template>
-                    </MenuButton>
-            </template>
-
-        </HomeHeader>
-        
-        <Section class="city-section">
-            твой чушпан
-            <div>
-                <img src="https://img.pikbest.com/png-images/qiantu/original-cute-cartoon-gopher_2736241.png!sw800" alt="" width="208">
-            </div>
-        </Section>
-        
-        <Section class="story-section">
-            статистика
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9c1904VH9PR2xYqTgQ0YAg9sSj2UAXtrbtQ&s" alt="">
-            </div>
-            <div>
-
-            </div>
-        </Section>
-
+            <Section class="achievements">
+                <div class="achievements-header">
+                    <div class="achievements-header-left">
+                        достижения
+                    </div>
+                    <div class="achievements-header-right">
+                        <a style="text-decoration: none;" href="???">        <!-- А куда??? -->
+                            смотреть всё>
+                        </a>
+                    </div>
+                </div>
+                <div class="blocks">
+                    <CityArea/>
+                    <CityArea/>
+                </div>
+            </Section>
       </div>
     </div>
 </template>
@@ -59,11 +50,31 @@
     font-size: 20px;
 }
 
+.cituselect{
+  position: absolute;
+  left: 70%;
+  top: 90px;
+  font-size: 8.26px;
+  background-color: #D9D9D9;
+  padding: 4px 2px 6px 3px;
+}
+
 .display{
     display: flex;
     flex-direction: column;
     gap: 16px;
     padding: 0px 20px;
+}
+
+.achievements-header{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.blocks{
+    display: flex;
+    gap: 16px;
 }
 
 </style>
