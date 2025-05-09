d5621af2a22247e66a38aac1de0fa5db9d80c5e9
Sooouuus <ten0305alexey@gmail.com>
2025-05-10 01:39:48 +0300

    для феди

---
+++ apps/web/components/Card.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+  title: string
+  description: string
+  image?: string  
+}>()
+</script>
+<template>
+    
+    <Block class="card-area-card">
+        <div class="card-call-area">
+            <div class="card-name-area">
+                <TwentyText>{{ title }}</TwentyText>
+                <TenText class="opacity">{{ description }}</TenText>
+            </div>
+            <div class="card-inf-area">
+                <TwelveText class="opacity"></TwelveText>
+            </div>
+        </div>
+        <ButtonAction class="white" @click="async() => {await navigateTo(`map/${id}`)}">забрать!</ButtonAction>
+        <!-- <img src="../public/images/Semen.png" class="city-area-card-background"> -->
+    </Block>
+    
+
+</template>
+<style scoped>
+.card-area-card {
+  background: radial-gradient(#FFE586, #FBFBFB);
+  background-position: bottom left;
+  border-radius: 15px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  transition: transform 0.3s ease;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.card-area-card:hover {
+  transform: translateY(-5px);
+}
+
+.card-name-area{
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+}
+.card-call-area{
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+.opacity{
+    color: #00000050;
+}
+.card-inf-area{
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    gap: 6px;
+}
+</style>
