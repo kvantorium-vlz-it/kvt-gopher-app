ff68246bae531fddc943aa3babe393801e9f0120
Sooouuus <ten0305alexey@gmail.com>
2025-05-10 18:35:22 +0300

    я посмотреть

---
+++ apps/web/pages/shop.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { getData, setData } from 'nuxt-storage/local-storage';
+
+const { fetchUser } = useStrapiAuth()
+const user = await fetchUser()
+const { find, findOne } = useStrapi()
+const cities = await findOne('cities',getData('cityId'))
+
+const name = user.value?.username!
+
+</script>
+
+
+<template >
+    <div class="main">
+        <TheHeader :username="name "/>
+        <div class="display">
+            <ButtonsShop>
+                <template #left>
+
+                    <MenuButton title="назад">
+                        <template #icon>
+                            <Icon name="tdesign:arrow-left"/>
+                        </template>
+                    </MenuButton>
+
+                </template>
+                <template #middle>
+                        <MenuButton title="костюмы">
+                        <template #icon>
+                            <Icon name="fa6-solid:shirt"/>
+                        </template>
+                    </MenuButton>
+                </template>
+            <template #right>
+                    <MenuButton title="обмен">
+                    <template #icon>
+                            <Icon name="fluent-emoji-high-contrast:counterclockwise-arrows-button"/>
+                        </template>
+                    </MenuButton>
+            </template>
+            
+            
+        </ButtonsShop>
+        
+      </div>
+    </div>
+</template>
+  
+<style scoped>
+.main {
+  font-size: 20px;
+    gap: 16px;
+    display: flex;
+    flex-direction: column;
+}
+
+.display{
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 0px 20px;
+}
+
+</style>
+  
